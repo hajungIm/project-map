060bb919c8cd6d9375400f7e965472ccd8da83e3
hajungIm <ihj_ewha@ewhain.net>
2023-08-14 18:50:33 +0900

    #1 - 스프링 부트 시작하기

---
+++ src/main/java/com/springstudy/projectmap/ProjectMapApplication.java
@@ -0,0 +1,13 @@
+package com.springstudy.projectmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMapApplication.class, args);
+    }
+
+}
