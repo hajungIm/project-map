42b23827eba43bac75c302e1274232cc151a5fba
hajungIm <ihj_ewha@ewhain.net>
2023-08-25 13:12:05 +0900

    #10 - 뷰 구현
    main 페이지에서 주소 입력 받을 때,
    카카오에서 제공하는 우편번호 서비스를 사용함.
    주소 입력란을 클릭하면 팝업을 노출하는 방식.
    * https://postcode.map.daum.net/guide
    
    뷰에서 전달하고 전달받을 때에는 엔티티 자체를 쓰기 보다
    Dto 를 만드는것이 좋으므로
    InputDto와 OutputDto을 생성 후 사용.
    
    길찾기 url과 로드뷰url은 추후 구현예정.

---
+++ src/main/java/com/springstudy/projectmap/direction/dto/InputDto.java
@@ -0,0 +1,11 @@
+package com.springstudy.projectmap.direction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class InputDto {
+    private String address;
+}
--- src/main/java/com/springstudy/projectmap/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/springstudy/projectmap/pharmacy/service/PharmacyRecommendationService.java
@@ -3,6 +3,7 @@ package com.springstudy.projectmap.pharmacy.service;
 import com.springstudy.projectmap.api.dto.DocumentDto;
 import com.springstudy.projectmap.api.dto.KakaoApiResponseDto;
 import com.springstudy.projectmap.api.service.KakaoAddressSearchService;
+import com.springstudy.projectmap.direction.dto.OutputDto;
 import com.springstudy.projectmap.direction.entity.Direction;
 import com.springstudy.projectmap.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,20 +24,36 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
-//        List<Direction> directionList = directionService.buildDirectionList(documentDto); // 공공기관에서 제공받은 데이터 이용
-        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto); // 카카오 api 이용
+        // 공공기관 약국 데이터 및 거리계산 알고리즘 이용
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
 
-        directionService.saveAll(directionList);
+        // kakao 카테고리를 이용한 장소 검색 api 이용
+        //List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(this::convertToOutputDto)
+                .collect(Collectors.toList());
+    }
+
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo")
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
